ae830ed121d41ee13922e52e0c7c6e0a85b67228
Arunkumar s <sarun4jun90@live.com>
2021-12-08 23:16:21 +0530

    Added Repo And DB Contexts

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogDbContext:ICatalogDBContext
+    {
+        public CatalogDbContext(IConfiguration Configuration)
+        {
+            var client = new MongoClient(Configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(Configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            var Products = database.GetCollection<Product>(Configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeeds.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ICatalogDBContext iCatalogDbContext;
+
+        public ProductRepository(ICatalogDBContext _iCatalogDbContext)
+        {
+            this.iCatalogDbContext = _iCatalogDbContext ?? throw new ArgumentNullException(nameof(_iCatalogDbContext));
+        }
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await iCatalogDbContext.Products.Find(p => true).ToListAsync();
+        }
+        public async Task<Product> GetProduct(string id)
+        {
+            return await iCatalogDbContext.Products.Find(p => p.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByName(string name)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Name , name);
+            return await iCatalogDbContext.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductsByCategory(string category)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, category);
+            return await iCatalogDbContext.Products.Find(filter).ToListAsync();
+        }
+
+        public async Task CreateProduct(Product product)
+        {
+            await iCatalogDbContext.Products.InsertOneAsync(product);
+        }
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+           var updateresult =  await iCatalogDbContext.Products.ReplaceOneAsync(filter: g=> g.Id == product.Id,replacement : product);
+           return updateresult.IsAcknowledged && updateresult.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.ElemMatch(p => p.Id, id);
+            var deletedResult = await iCatalogDbContext.Products.DeleteOneAsync(filter);
+            return deletedResult.IsAcknowledged && deletedResult.DeletedCount > 0;
+        }
+
+      
+    }
+}
